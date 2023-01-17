051fd3cf44c8fe7b18f9a13bfaba08a1ee228e91
Sam Hayes <sam.hayes@gamer-network.net>
2023-01-17 21:29:57 +0000

    Styling tweaks

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=optional"
+          rel="stylesheet preload"
+          as="style"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
