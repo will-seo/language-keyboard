af1943509434b65e1e49d2607c127ba805548984
Sam Hayes <sam.hayes@gamer-network.net>
2023-01-17 21:39:44 +0000

    Self-host fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import { Montserrat } from '@next/font/google';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 config.autoAddCss = false;
 
+const montserrat = Montserrat({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={montserrat.className}>
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,13 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=optional"
-          rel="stylesheet preload"
-          as="style"
-        ></link>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
