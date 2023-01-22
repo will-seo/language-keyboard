5ce84d26aeaa77e5a66e5e96f245ee712ff0beb8
will <97103929+will-seo@users.noreply.github.com>
2023-01-22 16:15:08 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,17 +18,26 @@ const HomePage = (props: PageProps) => {
         Accusamus dicta consequuntur harum quod necessitatibus odit perferendis a quo dolorem. Eius, placeat earum?
         Voluptatum, quia excepturi! Similique, debitis.
       </p>
-      <ul>
-        <li>
-          <a href="https://languagekeyboard.net/japanese">Japanese Keyboard</a>
-        </li>
-        <li>
-          <a href="https://languagekeyboard.net/russian">Russian Keyboard</a>
-        </li>
-        <li>
-          <a href="https://languagekeyboard.net/maori">Maori Keyboard</a>
-        </li>
-      </ul>
+      <table>
+        <tr>
+          <td>
+            <a href="https://languagekeyboard.net/japanese">Japanese Keyboard</a>
+          </td>
+          <td>
+            <a href="https://languagekeyboard.net/russian">Russian Keyboard</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <li>
+              <a href="https://languagekeyboard.net/japanese">Japanese Keyboard</a>
+            </li>
+          </td>
+          <td>
+            <a href="https://languagekeyboard.net/russian">Russian Keyboard</a>
+          </td>
+        </tr>
+      </table>
     </Layout>
   );
 };
